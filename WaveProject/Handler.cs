82b1bd35ffa40e0c436e428c3836ff6b91bd0e15
massicottetyler <massicottetyler@gmail.com>
2015-10-03 22:46:13 -0700

    adding handler class

---
+++ WaveProject/Handler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaveProject
+{
+    public class Handler
+    {
+        private WavReader reader;
+        private WavWriter writer;
+        private DFT dft;
+
+        public Handler()
+        {
+
+        }
+    }
+}
